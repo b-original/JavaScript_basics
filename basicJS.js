41ea33e5d96132fd3c235d17a2326c1631b6e12c
Bhavya Varshney <guptabhavya0@gmail.com>
2023-01-19 22:29:16 +0530

    added promt and alert function basics

--- basicJS.js
+++ basicJS.js
@@ -1,2 +1,22 @@
 // the hello world program
-console.log('Hello World');
+console.log('Hello World from console!');
+
+//Below function displays an alert box over the current window with the specified message.
+//alert("Hello, World from alert!");
+
+// Below function is used when you want to print the content to the HTML document.
+//document.write('Hello, World!');
+
+//Since node does not support promt directly
+const prompt=require("prompt-sync")({sigint:true});
+
+// taking kilometers input from the user
+const kilometers = prompt("Enter value in kilometers: ")
+
+// conversion factor
+const factor = 0.621371
+
+// calculate miles
+const miles = kilometers * factor
+
+console.log(`${kilometers} kilometers is equal to ${miles} miles.`);
